b742383550b111a0f357aa7cf5943b7d689432b6
Guillermo Abneed Rodriguez Velazquez <Abneed@Guillermos-MacBook-Air.local>
2018-07-30 11:11:49 -0500

    Formulario para iniciar sesion.
    Nuevo:
    - Se diseño el formulario para iniciar sesion en la aplicación.
    - Se actualizo los paquetes de dependencias de este proyecto.

--- src/components/IniciarSesion.vue
+++ src/components/IniciarSesion.vue
@@ -1,8 +1,8 @@
-<template>
+<template id="signin-page">
     <ons-page>
-      <ons-toolbar>
-        <div class="center">Iniciar sesión</div>
-        <div class="right"><ons-toolbar-button>Cerrar</ons-toolbar-button></div>
+      <ons-toolbar modifier="night">
+        <div class="center"><b>Fake Uber</b></div>
+        <!-- <div class="right"><ons-toolbar-button modifier="night">Cerrar</ons-toolbar-button></div> -->
       </ons-toolbar>
 
       <div class="login-form">
@@ -10,13 +10,75 @@
         <input type="password" class="text-input--material" placeholder="Contraseña" value="">
         <br><br>
         <ons-button modifier="large" class="login-button" @click="$ons.notification.alert('Iniciando sesión...')">Iniciar sesión</ons-button>
-        <br><br>
-        <ons-button modifier="quiet" class="forgot-password">¿Se te olvidó tu contraseña?</ons-button><ons-button modifier="quiet" class="forgot-password">Regístrate</ons-button>
+        <div class="_1rf5">
+          <span class="_1rf8">o</span>
+        </div>
+        <ons-button modifier="simple" class="forgot-password"><b>Crear una cuenta nueva</b></ons-button><ons-button modifier="quiet" class="forgot-password">¿Se te olvidó tu contraseña?</ons-button>
       </div>
     </ons-page>
 </template>
 
 <style>
+
+._1rf5 {
+    display: block;
+    margin-bottom: 10px;
+    margin-left: 10.5%;
+    margin-right: 10.5%;
+    margin-top: 10px;
+    overflow: hidden;
+    text-align: center;
+    white-space: nowrap;
+    width: 79%;
+}
+._1rf5>span {
+    display: inline-block;
+    position: relative;
+}
+._1rf8 {
+    color: #4b4f56;
+}
+._1rf5>span:before {
+    margin-right: 15px;
+    right: 100%;
+}
+._1rf5>span:after {
+    left: 100%;
+    margin-left: 15px;
+}
+._1rf5>span:before, ._1rf5>span:after {
+    background: #ced0d4;
+    content: "";
+    height: 1px;
+    position: absolute;
+    top: 50%;
+    width: 9999px;
+}
+
+.toolbar--night {
+  background-color: #333333;
+}
+
+.toolbar-button--night {
+  color:white;
+  font-style:oblique;
+}
+
+.toolbar--night div{
+  color: white;
+}
+
+.button--simple {
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    width: 200px;
+    border-radius: 10px;
+    background-color: hsl(141, 71%, 48%);
+    /* background-color: hsl(120, 100%, 40%); */
+}
+
 input[type=text], input[type=password] {
   display: block;
   width: 100%;
--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import 'onsenui/css/onsen-css-components.css';
 // JS import
 import Vue from 'vue'
 import VueOnsen from 'vue-onsenui'; // This already imports 'onsenui'
-import App from './components/Registrate.vue'
+import App from './components/IniciarSesion.vue'
 
 Vue.use(VueOnsen); // Plugin VueOnsen to Vue.
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+    render: h => h(App)
 }).$mount('#app')
